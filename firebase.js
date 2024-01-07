b8a63e8032f203755665e6dcfe52d96161b18c07
Paulina <paulalastowiecka@gmail.com>
2024-01-07 18:14:21 +0100

    prod

--- firebase.js
+++ firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app"
 import { getFirestore, collection } from "firebase/firestore"
 
 const firebaseConfig = {
-	apiKey: "AIzaSyC0SV4h02si538fw40ylJrix3wZNUEDxuk",
-	authDomain: "react-notes-2959c.firebaseapp.com",
-	projectId: "react-notes-2959c",
-	storageBucket: "react-notes-2959c.appspot.com",
-	messagingSenderId: "608729515472",
-	appId: "1:608729515472:web:8a9a6f8c5efac0abce65d5",
+	apiKey: process.env.REACT_APP_API_KEY,
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+	projectId: process.env.REACT_APP_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
